875c4bce19b014169fda1eff616b3ed980b37979
Edison Yang <edison.cy.yang@gmail.com>
2016-06-27 16:59:06 -0700

    Change app module name

--- www/js/app.js
+++ www/js/app.js
@@ -5,9 +5,9 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase'])
+angular.module('mychat', ['ionic', 'mychat.controllers', 'mychat.services', 'firebase'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('mychat.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
